b3825b869796f17b43af1a03e7cbdadec4e21df0
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-02-24 22:37:15 -0300

    feat: create user services

---
+++ users/services.ts
@@ -0,0 +1,18 @@
+import { prisma } from '../prisma/prisma-client'
+import { hash } from 'bcrypt'
+import { UserType } from './entities/User'
+
+export const SALT_OR_ROUNDS = 8
+const create = async ({ name, password, phone, id, rulles }: UserType) => {
+  const passwordHash = await hash(password, SALT_OR_ROUNDS)
+
+  const user = await prisma.user.create({
+    data: { name, password: passwordHash, phone, id, rulles },
+    select: { id: true, name: true, phone: true, rulles: true },
+  })
+
+  return user
+}
+
+const userServices = { create }
+export default userServices
