9fb3256ea9dd150b0e093539d1543a72ad47a2ff
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-02-24 23:26:34 -0300

    chore: change dir pattern

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,8 @@ app.use(express.json())
 
 app.use(routes)
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: Error, req: Request, res: Response, _next: NextFunction) => {
   if (err instanceof ErrorBase) {
     res.status(err.statusCode).json(err.toJSON())
     return
--- src/users/services.ts
+++ src/users/services.ts
@@ -1,4 +1,4 @@
-import { prisma } from '../prisma/prisma-client'
+import { prisma } from '../../prisma/prisma-client'
 import { hash } from 'bcrypt'
 import { UserType } from './entities/User'
 
