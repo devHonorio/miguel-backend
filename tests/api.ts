e7e8ebd5c3b290cd9cd4af53a7f836bfa954a5d3
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-03-02 18:31:25 -0300

    feat: add methods `auth` and `authAdmin` and define `baseUrl`

--- tests/api.ts
+++ tests/api.ts
@@ -1,12 +1,50 @@
-const headers = { 'Content-type': 'application/json' }
-export const CreateApiClient = (url: string) => {
-  const post = async <T>(route: string, data: T) => {
-    return await fetch(url + route, {
+interface HeadersTypes {
+  type?: 'Bearer'
+  token?: string
+}
+
+const headers = (auth?: HeadersTypes) => {
+  const authorization = () => {
+    if (auth?.type === 'Bearer')
+      return { Authorization: `${auth?.type} ${auth?.token}` }
+    if (auth?.token) return { Authorization: auth?.token }
+  }
+  return {
+    'Content-type': 'application/json',
+    ...authorization(),
+  }
+}
+
+const baseUrl = 'http://localhost:3001'
+
+export const CreateApiClient = () => {
+  const post = async <T>(route: string, data: T, auth?: HeadersTypes) => {
+    return await fetch(baseUrl + route, {
       method: 'POST',
-      headers,
+      headers: headers(auth),
       body: JSON.stringify(data),
     })
   }
 
-  return { post }
+  const authAdmin = async () => {
+    const response = await fetch(baseUrl + '/login', {
+      method: 'POST',
+      headers: headers(),
+      body: JSON.stringify({ phone: '00000000000', password: '0000' }),
+    })
+
+    return (await response.json()) as { access_token: string }
+  }
+
+  const auth = async () => {
+    const response = await fetch(baseUrl + '/login', {
+      method: 'POST',
+      headers: headers(),
+      body: JSON.stringify({ phone: '11111111111', password: '1111' }),
+    })
+
+    return (await response.json()) as { access_token: string }
+  }
+
+  return { post, authAdmin, auth }
 }
