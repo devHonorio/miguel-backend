4d40bc2f16c9965f8ed6116af8c58ae9d0c5c7f7
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-03-07 15:54:27 -0300

    feat: crate entitie Cup

---
+++ src/cups/entities/Cup.ts
@@ -0,0 +1,36 @@
+import z, { ZodError } from 'zod'
+import { BadRequestError } from '../../errors/error-base'
+
+const cupSchema = z.object({
+  size: z
+    .number({
+      invalid_type_error: 'Tamanho do copo deve ser um numero inteiro.',
+    })
+    .int('Tamanho do copo deve ser um numero inteiro.'),
+})
+
+export type CupType = z.infer<typeof cupSchema>
+
+function create(cup: CupType) {
+  try {
+    const { size } = cupSchema.parse(cup)
+
+    return {
+      size,
+    }
+  } catch (error) {
+    const err = error as ZodError
+
+    throw new BadRequestError({
+      message: err.issues[0].message,
+      action: `Verifique se a propiedade "${err.issues[0].path}" está correta.`,
+      cause: error,
+    })
+  }
+}
+
+const Cup = {
+  create,
+}
+
+export default Cup
