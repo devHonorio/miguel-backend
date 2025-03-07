91e6f147d98f3ea4ca26042ea09791976d705818
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-03-07 15:56:32 -0300

    feat: crate middleware of autorization write cups

---
+++ src/cups/middleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express'
+import { UnauthorizedError } from '../errors/error-base'
+
+const authorization = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const user = req.user
+
+  if (!user)
+    throw new UnauthorizedError({
+      message: 'Usuário não encontrado.',
+      action: 'Verifique o token.',
+    })
+
+  const { rulles } = user
+
+  if (!rulles.includes('write:cups'))
+    throw new UnauthorizedError({
+      action: 'Verifique se usuário tem rulle "write:cups".',
+      message: 'Usuário não autorizado.',
+    })
+
+  next()
+}
+
+const cupsMiddlewares = { authorization }
+
+export default cupsMiddlewares
