e6d4fd663c934bcb7882c72fe236cd9008b606f2
José Honorio de Oliveira Sobrinho Neto <honorio.dev@gmail.com>
2025-04-28 12:02:00 -0300

    fix: add user_id

--- src/address/controller.ts
+++ src/address/controller.ts
@@ -5,7 +5,10 @@ import addressServices from './services'
 const create: RequestHandler = async (req, res) => {
   const addressBody = Address.create(req.body)
 
-  const address = await addressServices.create(addressBody)
+  const address = await addressServices.create({
+    ...addressBody,
+    user_id: req.user?.id,
+  })
 
   res.status(201).json(address)
 }
